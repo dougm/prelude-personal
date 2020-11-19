cfc196cc89771a9e6b09eeeee8b5f683d5ee59e8
Doug MacEachern <dougm@vmware.com>
2020-11-18 19:52:40 -0800

    minimal ui

--- preload/dougm.el
+++ preload/dougm.el
@@ -1,5 +1,6 @@
 (when window-system
   (require 'prelude-packages)
   (scroll-bar-mode -1)
+  (setq prelude-minimalistic-ui t)
   (prelude-require-package 'solarized-theme)
   (setq prelude-theme 'solarized-dark))
